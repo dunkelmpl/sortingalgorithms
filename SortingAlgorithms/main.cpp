f9727152cf5317bc017b6302b36b609bcac7f35b
Pavel Martynau <dunkel@tut.by>
2017-11-28 23:32:32 -0800

    Added InsertionSort to the list of testing instances.

--- SortingAlgorithms/main.cpp
+++ SortingAlgorithms/main.cpp
@@ -1,11 +1,23 @@
 #include "SortingTester.h"
 #include "BubbleSort.h"
 #include "SelectionSort.h"
+#include "InsertionSort.h"
 
 int main()
 {
-    return (
-        0 == SortingTester::run(new BubbleSort) && 
-        0 == SortingTester::run(new SelectionSort)
-    );
+    int result = 0;
+
+    vector<AbstractSort*> sortTestInstances{
+        new BubbleSort(),
+        new SelectionSort(),
+        new InsertionSort()
+    };
+
+    for (auto&& sortTest : sortTestInstances) {
+        result = (0 == result) && SortingTester::run(sortTest);
+
+        delete sortTest;
+    }
+
+    return result;
 }
