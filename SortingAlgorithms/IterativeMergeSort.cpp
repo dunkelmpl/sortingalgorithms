26f05a0e20397d5c6ea2d19ce7aae2a0c4b7fc64
Pavel Martynau <dunkel@tut.by>
2017-11-30 01:35:12 -0800

    Implemented iterative merge sort.

---
+++ SortingAlgorithms/IterativeMergeSort.cpp
@@ -0,0 +1,25 @@
+#include "IterativeMergeSort.h"
+#include <algorithm>
+
+void IterativeMergeSort::sort()
+{
+    for (size_t size = 1, dblSize = size << 1; ; size = dblSize, dblSize = size << 1) {
+
+        for (size_t left = 0; left < storage.size(); left += dblSize) {
+            size_t middle = left + size - 1;
+            size_t right = middle + size;
+
+            if (right >= storage.size()) {
+                right = storage.size() - 1;
+            }
+
+            if (middle < right) {
+                this->merge(left, middle, right);
+            }
+        }
+
+        if (size >= storage.size()) {
+            break;
+        }
+    }
+}
---
+++ SortingAlgorithms/IterativeMergeSort.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "MergeSort.h"
+
+class IterativeMergeSort : public MergeSort
+{
+public:
+    void sort();
+};
