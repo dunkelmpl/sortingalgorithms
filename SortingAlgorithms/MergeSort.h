2edd0e9445d4ac8e8bd65b89a1708fa62f1fdf6b
Pavel Martynau <dunkel@tut.by>
2017-11-29 21:53:40 -0800

    Implemented merge sort.

---
+++ SortingAlgorithms/MergeSort.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "BaseSort.h"
+
+class MergeSort : public BaseSort
+{
+public:
+    void sort();
+protected:
+    void sortChunk(size_t start, size_t end);
+    void merge(size_t start, size_t middle, size_t end);
+};
+
